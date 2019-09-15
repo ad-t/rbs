2141db2450ec8740daaf50d2087bd7508a06ad7b
Adam T <ad-t@users.noreply.github.com>
2019-09-15 20:19:11 +1000

    Responses swagger doc section works now

--- src/entity/seat.ts
+++ src/entity/seat.ts
@@ -1,6 +1,12 @@
 import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
 import {Show} from "./show";
 
+export enum SeatStatus {
+  AVAILABLE = "available",
+  RESERVED = "reserved",
+  PURCHASED = "purchased"
+}
+
 @Entity()
 export class Seat {
   @PrimaryGeneratedColumn()
@@ -11,6 +17,13 @@ export class Seat {
 
   @Column()
   public seatNumber: number;
+
+  @Column({
+    default: SeatStatus.AVAILABLE,
+    enum: SeatStatus,
+    type: "enum",
+  })
+  public status: SeatStatus;
 
   @ManyToOne((type) => Show, (show) => show.seats)
   public show: Show;
--- src/entity/show.ts
+++ src/entity/show.ts
@@ -2,12 +2,6 @@ import {Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "type
 import {Production} from "./production";
 import {Seat} from "./seat";
 
-export enum SeatStatus {
-  AVAILABLE = "available",
-  RESERVED = "reserved",
-  PURCHASED = "purchased"
-}
-
 @Entity()
 export class Show {
   @PrimaryGeneratedColumn()
@@ -18,13 +12,6 @@ export class Show {
 
   @Column()
   public time: Date;
-
-  @Column({
-    default: SeatStatus.AVAILABLE,
-    enum: SeatStatus,
-    type: "enum",
-  })
-  public status: SeatStatus;
 
   @ManyToOne((type) => Production, (production) => production.shows)
   public production: Production;
