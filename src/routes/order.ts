4fb2fc990ce799b9a3748cd20c76efcd5456c40f
Dominic <he-lium@users.noreply.github.com>
2019-09-22 18:42:14 +1000

    Refactor and save paypal order details

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -3,20 +3,23 @@ import currency = require("currency.js");
 import {Request, Response} from "express";
 import { getConnection } from "typeorm";
 import { Order } from "../entity/order";
+import { PaypalOrder } from "../entity/paypal_order";
 import Logger from "../logging";
-import { paypalClient, paypalFee } from "../services/paypal";
-
-// TODO rewrite this if we ever expand
-const defaultTZ = "Australia/Sydney";
+import { orderCreateRequestBody, paypalClient, paypalFee } from "../services/paypal";
 
 export async function GetOrder(req: Request, res: Response): Promise<void> {
-  const conn = getConnection();
-  const order: Order = await conn.getRepository(Order).findOne({id: req.params.id}, {relations: ["show"]});
-  if (!order) {
-    res.status(404).json({error: `No order found with id ${req.params.id}`});
-    return;
+  try {
+    const conn = getConnection();
+    const order: Order = await conn.getRepository(Order).findOne({id: req.params.id}, {relations: ["show"]});
+    if (!order) {
+      res.status(404).json({error: `No order found with id ${req.params.id}`});
+      return;
+    }
+    res.json(order);
+  } catch (err) {
+    Logger.Error(err.stack);
+    res.status(500).json({error: "Internal server error"});
   }
-  res.json(order);
 }
 
 export async function SetupPaypal(req: Request, res: Response) {
@@ -36,6 +39,9 @@ export async function SetupPaypal(req: Request, res: Response) {
       res.status(404).json({error: `No order found with id ${req.params.id}`});
       return;
     }
+    if (order.paid === true) {
+      res.status(409).json({error: "Order is already paid"});
+    }
     const show = order.show;
     const prod = show.production;
 
@@ -43,47 +49,30 @@ export async function SetupPaypal(req: Request, res: Response) {
     const unitPrice = AUD(show.seatPrice);
     const fee = paypalFee(subtotal);
     const totalPrice = subtotal.add(fee);
-    Logger.Info(`sub=${subtotal}, fee=${fee}, total=${totalPrice}`);
 
     // https://developer.paypal.com/docs/checkout/reference/server-integration/set-up-transaction/#on-the-server
     const request = new paypal.orders.OrdersCreateRequest();
     request.prefer("return=representation");
-    const details = {
-      intent: "CAPTURE",
-      application_context: {
-        brand_name: "Revue Booking System",
-        shipping_preferences: "NO_SHIPPING",
-        user_action: "PAY_NOW"
-      },
-      purchase_units: [{
-        custom_id: order.id,
-        description: "Revue Tickets",
-        soft_descriptor: "RBS Revue Tickets", // credit card statement
-        amount: {
-          currency_code: "AUD",
-          value: totalPrice.format(),
-          breakdown: {
-            item_total: moneyObj(subtotal),
-            handling: moneyObj(fee)
-          }
-        },
-        items: [{
-          name: `${prod.title} ${prod.year}: ${prod.subtitle}`,
-          description: show.time.toLocaleString("en-AU", {timeZone: defaultTZ}),
-          category: "DIGITAL_GOODS",
-          quantity: order.numSeats,
-          unit_amount: moneyObj(unitPrice)
-        }]
-      }]
-    };
-
+    const details = orderCreateRequestBody(
+      order.id, prod.title, prod.year, prod.subtitle, show.time, subtotal,
+      unitPrice, order.numSeats);
     request.requestBody(details);
     Logger.Info(JSON.stringify(details));
 
     // Send request to paypal api
-    const paypalOrder = await paypalClient().execute(request);
-    Logger.Info(JSON.stringify(paypalOrder));
-    res.json(paypalOrder);
+    const paypalResponse = await paypalClient().execute(request);
+    Logger.Info(JSON.stringify(paypalResponse));
+
+    // Save paypal details
+    const paypalOrder = new PaypalOrder();
+    // paypalOrder.order = order;
+    paypalOrder.totalPrice = (totalPrice.format() as any);
+    paypalOrder.id = paypalResponse.result.id;
+    paypalOrder.captureID = null;
+    paypalOrder.order = order;
+    await conn.getRepository(PaypalOrder).save(paypalOrder);
+
+    res.json({paypalOrderID: paypalOrder.id});
   } catch (err) {
     Logger.Error(err.stack);
     res.status(500).json({error: "Internal server error"});
@@ -97,10 +86,6 @@ export async function PaypalCaptureOrder(req: Request, res: Response) {
     Logger.Error(err.stack);
     res.status(500).json({error: "Internal server error"});
   }
-}
-
-function moneyObj(amount: currency): object {
-  return {currency_code: "AUD", value: amount.format()};
 }
 
 function AUD(amount: any): currency {
