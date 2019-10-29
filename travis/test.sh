f2cd1f1688a867ad526290fa053d5498f7991aab
Adam T <ad-t@users.noreply.github.com>
2019-10-29 23:37:57 +1100

    Added a script that travis will call to set up and run the tests correctly

---
+++ travis/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# check if it builds correctly
+yarn build
+
+if [ $? -ne 0 ]
+then
+  echo "Build failed"
+  exit
+fi
+
+# start serving
+yarn serve &
+
+# wait for some time for the server to start
+sleep 10
+
+# run the tests
+yarn ci_test
+
+# once complete, clean up the running server
+killall yarn
