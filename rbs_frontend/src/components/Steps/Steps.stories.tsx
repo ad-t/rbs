2cc5fbf7b1ef93e1cff31ebd8e8043ab293b5bc0
Robustprogram <robustprogram@users.noreply.github.com>
2021-04-28 21:53:42 +1000

    Remove default as it is not an useful story

--- rbs_frontend/src/components/Steps/Steps.stories.tsx
+++ rbs_frontend/src/components/Steps/Steps.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { StepItemState } from 'src/shared/enums';
 import Steps from './Steps';
 import StepItem from './StepItem';
 import StepController from './Steps.controller';
@@ -9,15 +8,6 @@ import { createSteps } from './create';
 export default {
   title: 'Component/Steps',
   component: Steps,
-};
-
-export const Default: Story = () => {
-  const { StepsElement } = createSteps([
-    <StepItem icon="0" name="Zero" />,
-    <StepItem icon="1" name="First" />,
-    <StepItem icon="2" name="Second" />,
-  ]);
-  return <StepsElement />;
 };
 
 export const Modifiable: Story = () => {
