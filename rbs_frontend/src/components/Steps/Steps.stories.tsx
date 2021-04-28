94ea1df8636f1c7faba77ceff58727b3862f9275
Robustprogram <robustprogram@users.noreply.github.com>
2021-04-28 21:43:33 +1000

    Create a basic step feature with modifiable state

--- rbs_frontend/src/components/Steps/Steps.stories.tsx
+++ rbs_frontend/src/components/Steps/Steps.stories.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import { StepItemState } from 'src/shared/enums';
 import Steps from './Steps';
+import StepItem from './StepItem';
+import { createSteps } from './create';
 
 export default {
   title: 'Component/Steps',
   component: Steps,
 };
 
-const Template: Story = () => <Steps />;
+export const Default: Story = () => {
+  const { StepsElement } = createSteps([
+    <StepItem icon="0" name="Zero" />,
+    <StepItem icon="1" name="First" />,
+    <StepItem icon="2" name="Second" />,
+  ]);
+  return <StepsElement />;
+};
 
-export const Default = Template.bind({});
+export const Modifiable: Story = () => {
+  const { StepsElement, stepsState } = createSteps([
+    <StepItem icon="0" name="Zero" />,
+    <StepItem icon="1" name="First" />,
+    <StepItem icon="2" name="Second" />,
+  ]);
+
+  const addProgress = () => {
+    stepsState.itemsProgress = [
+      ...stepsState.itemsProgress,
+      StepItemState.IN_PROGRESS,
+    ];
+  };
+
+  const removeProgress = () => {
+    if (stepsState.itemsProgress.length) {
+      stepsState.itemsProgress.pop();
+      stepsState.itemsProgress = [...stepsState.itemsProgress];
+    }
+  };
+
+  return (
+    <>
+      <StepsElement />
+      <button onClick={addProgress}>Advance</button>
+      <button onClick={removeProgress}>Retreat</button>
+    </>
+  );
+};
--- rbs_frontend/src/components/Steps/Steps.tsx
+++ rbs_frontend/src/components/Steps/Steps.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
-import { IoTicket } from 'react-icons/io5';
-import { FaCreditCard, FaFileInvoice } from 'react-icons/fa';
-
-import Item from './StepItem';
+import { StepItemState } from 'src/shared/enums';
 
 const Wrapper = styled.div`
   display: flex;
 `;
 
-export default function Steps() {
-  return (
-    <Wrapper>
-      <Item icon={<IoTicket />} name="Purchase Ticket" />
-      <Item icon={<FaCreditCard />} name="Payment" />
-      <Item icon={<FaFileInvoice />} name="Invoice" />
-    </Wrapper>
-  );
+interface StepsProps extends React.HTMLAttributes<HTMLDivElement> {
+  stepProgress?: StepItemState[];
+}
+
+/**
+ * Go through the list of step items state and assign them to a child element base on the index
+ */
+function matchStateToChild(
+  children: React.ReactNode,
+  stepProgress: StepItemState[]
+) {
+  return React.Children.map(children, (child, index) => {
+    if (!child) return null;
+    return React.cloneElement(child as JSX.Element, {
+      state:
+        index < stepProgress.length
+          ? stepProgress[index]
+          : StepItemState.NOT_STARTED,
+    });
+  });
+}
+
+export default function Steps({ children, stepProgress }: StepsProps) {
+  const assignedChildren = stepProgress
+    ? matchStateToChild(children, stepProgress)
+    : children;
+
+  return <Wrapper>{assignedChildren}</Wrapper>;
 }
