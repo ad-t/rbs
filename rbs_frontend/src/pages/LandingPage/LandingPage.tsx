dc87a470464260111c1a29f778c4a359d0c91d57
Robustprogram <robustprogram@users.noreply.github.com>
2021-06-08 22:15:09 +1000

    Update landing page

--- rbs_frontend/src/pages/LandingPage/LandingPage.tsx
+++ rbs_frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,43 +1,104 @@
-/*
- * This file will handle the entire landing page.
- */
 import React from 'react';
+import { LoremIpsum } from 'lorem-ipsum';
 import { Link } from 'react-router-dom';
-import { Button, Image } from 'semantic-ui-react';
+import YouTube from 'react-youtube';
+import { Button } from 'semantic-ui-react';
+import styled from 'styled-components';
+import * as variables from 'src/shared/css.variables';
+import Background from './Background.png';
+import LogoSrc from './logo.png';
 
-export default class LandingPage extends React.Component {
-  render() {
-    return (
-      <div className="landing-page" style={{ flexGrow: 1 }}>
-        <Image
-          src="/logo.png"
-          size="medium"
-          centered
-          style={{ paddingTop: '4em' }}
-        ></Image>
-        <Image
-          src="/breaking-bones.png"
-          centered
-          style={{ paddingTop: '4em' }}
-        ></Image>
+const Logo = styled.img`
+  margin-bottom: 1rem;
+  width: 152px;
+`;
 
-        <div className="button-group">
-          {/*
-        <Container text align='center' style={{color:'white', margin: '1em', fontSize: '20px', fontWeight: 'bold'}}>
-          <p>Breaking Bones:</p>
-          <p>The comedy sketch spectacular returning to UNSW!</p>
-          <p>13-16 April 2021</p>
-        </Container>
-        */}
-          <Link to="/tickets">
-            <Button primary>Buy Tickets</Button>
-          </Link>
-          {/*
-          <Button secondary>About The Show</Button>
-          <Button secondary>Watch Trailer</Button>
-        */}
-        </div>
-      </div>
-    );
+const PageWrapper = styled.div`
+  background: url(${Background});
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+  width: 100vw;
+`;
+
+const LandingPageWrapper = styled.div`
+  box-sizing: border-box;
+  display: flex;
+
+  @media (max-width: ${variables.mediaSmall}) {
+    flex-flow: column;
   }
+`;
+
+const VideoColumn = styled.div`
+  iframe {
+    border-bottom-left-radius: 50px;
+    overflow: hidden;
+
+    @media (max-width: ${variables.mediaSmall}) {
+      width: 356px;
+    }
+  }
+`;
+
+const ProductionDescription = styled.div`
+  color: ${variables.grey100};
+
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+
+  width: 512px;
+
+  @media (max-width: ${variables.mediaSmall}) {
+    width: 100%;
+  }
+`;
+
+const Description = styled.div`
+  padding-bottom: 1rem;
+`;
+
+const Gap = styled.div`
+  padding: 1rem;
+`;
+
+export default function LandingPage() {
+  const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4,
+    },
+    wordsPerSentence: {
+      max: 8,
+      min: 2,
+    },
+  });
+  return (
+    <PageWrapper>
+      <LandingPageWrapper>
+        <ProductionDescription>
+          <Description>
+            <div style={{ textAlign: 'center' }}>
+              <Logo src={LogoSrc} alt="CSE Revue" />
+            </div>
+            <p>{lorem.generateParagraphs(1)}</p>
+            <p>{lorem.generateParagraphs(1)}</p>
+          </Description>
+          <Link to="/tickets">
+            <Button fluid primary>
+              Buy Tickets
+            </Button>
+          </Link>
+        </ProductionDescription>
+        <Gap />
+        <VideoColumn>
+          <YouTube videoId="dQw4w9WgXcQ" />
+        </VideoColumn>
+      </LandingPageWrapper>
+    </PageWrapper>
+  );
 }
